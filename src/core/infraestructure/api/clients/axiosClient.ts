24cefd348718d21241fab44d5805d43ea39bb2fa
Cristhian <ccdelgadop@gmail.com>
2025-03-30 09:04:51 -0500

    add redux and axios

---
+++ src/core/infraestructure/api/clients/axiosClient.ts
@@ -0,0 +1,33 @@
+// axiosClient.ts
+import axios from "axios";
+import { API_BASE_URL } from "../../../config/env";
+
+const axiosClient = axios.create({
+  baseURL: API_BASE_URL,
+  timeout: 6000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Interceptors for Auth tokens, error handling, logging, etc.
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axiosClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      // handle unauthorized errors globally
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosClient;
