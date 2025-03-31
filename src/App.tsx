e55184b84bf845d8106bf448715c5db1199155a9
Cristhian <ccdelgadop@gmail.com>
2025-03-30 21:43:34 -0500

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,14 @@
-import { useState } from "react";
-import { Button, Typography, Container } from "@mui/material";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes";
+import { CssBaseline } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <Container>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <Typography variant="h1" gutterBottom>
-        Vite + React
-      </Typography>
-      <div className="card">
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </Button>
-        <Typography variant="body1" mt={2}>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </Typography>
-      </div>
-      <Typography className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </Typography>
-    </Container>
+    <BrowserRouter>
+      <CssBaseline />
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
